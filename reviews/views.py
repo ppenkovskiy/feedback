0c5a7184f15445fbadf9b496beb44546bbfc92e3
Pavel Penkovskiy <p.penkovskiy@gmail.com>
2023-10-17 15:17:45 +0300

    6

--- reviews/views.py
+++ reviews/views.py
@@ -6,15 +6,18 @@ from .forms import ReviewForm
 def review(request):
     if request.method == 'POST':
         form = ReviewForm(request.POST)
+
         if form.is_valid():
             print(form.cleaned_data)
             return HttpResponseRedirect('/thank-you')
 
-    form = ReviewForm()
+    else:
+        form = ReviewForm()
 
     return render(request, 'reviews/review.html', {
         'form': form
     })
+
 
 
 def thank_you(request):

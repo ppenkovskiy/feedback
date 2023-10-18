a9d052bc1688489bc6dc4d592499004c24558bc1
Pavel Penkovskiy <p.penkovskiy@gmail.com>
2023-10-18 17:41:56 +0300

    9

--- reviews/models.py
+++ reviews/models.py
@@ -5,3 +5,4 @@ class Review(models.Model):
     user_name = models.CharField(max_length=100)
     review_text = models.TextField()
     rating = models.IntegerField()
+    owner_comment = models.TextField(default='')

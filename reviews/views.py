542d89c905dc853c9503b17b91cf38bd9fac51e5
Pavel Penkovskiy <p.penkovskiy@gmail.com>
2023-10-18 18:35:07 +0300

    11

--- reviews/views.py
+++ reviews/views.py
@@ -1,20 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .forms import ReviewForm
-from .models import Review
 
 
 def review(request):
     if request.method == 'POST':
         form = ReviewForm(request.POST)
+
         if form.is_valid():
-            review = Review(user_name=form.cleaned_data['user_name'],
-                            review_text=form.cleaned_data['review_text'],
-                            rating=form.cleaned_data['rating'])
-            review.save()
+            form.save()
             return HttpResponseRedirect('/thank-you')
+
     else:
         form = ReviewForm()
+
     return render(request, 'reviews/review.html', {
         'form': form
     })

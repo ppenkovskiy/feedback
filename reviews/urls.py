91a2a321ba0aee884123d6057daa105702d64241
Pavel Penkovskiy <p.penkovskiy@gmail.com>
2023-10-18 19:02:14 +0300

    12

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-from .views import review
-from django.urls import path, include
 from . import views
 
 
 urlpatterns = [
-    path("", views.review),
+    path("", views.ReviewView.as_view()),
     path('thank-you/', views.thank_you)
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,22 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .forms import ReviewForm
+from django.views import View
 
 
-def review(request):
-    if request.method == 'POST':
+class ReviewView(View):
+    def get(self, request):
+        form = ReviewForm()
+
+        return render(request, 'reviews/review.html', {
+            'form': form
+        })
+
+    def post(self, request):
         form = ReviewForm(request.POST)
-
         if form.is_valid():
             form.save()
             return HttpResponseRedirect('/thank-you')
-
-    else:
-        form = ReviewForm()
-
-    return render(request, 'reviews/review.html', {
-        'form': form
-    })
+        return render(request, 'reviews/review.html', {
+            'form': form
+        })
 
 
 def thank_you(request):

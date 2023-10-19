2e5c8aacac1e84bc4604c494eef64155fdc38691
Pavel Penkovskiy <p.penkovskiy@gmail.com>
2023-10-19 18:39:40 +0300

    15

--- reviews/views.py
+++ reviews/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect, request
 from .forms import ReviewForm
 from django.views import View
 from django.views.generic.base import TemplateView
+from .models import Review
 
 
 class ReviewView(View):
@@ -29,4 +30,31 @@ class ThankYouView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['message'] = 'Great work!'
+        return context
+
+
+class ReviewsListView(TemplateView):
+    template_name = 'reviews/review_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        reviews = Review.objects.all()
+        context['reviews'] = reviews
+        return context
+
+
+# def review_detail(request, slug):
+#     identified_review = get_object_or_404(Review, slug=slug)
+#     return render(request, 'reviews/review_detail.html',
+#                   {'review': identified_review,
+#                    })
+
+class ReviewDetailView(TemplateView):
+    template_name = 'reviews/review_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        review_id = kwargs.get('id')
+        selected_review = Review.objects.get(id=review_id)
+        context['review'] = selected_review
         return context

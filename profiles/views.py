9cbc793ef754ee8b2e3f99cbe378bfeda9bf898c
Pavel Penkovskiy <p.penkovskiy@gmail.com>
2023-10-20 16:46:38 +0300

    25

--- profiles/views.py
+++ profiles/views.py
@@ -3,23 +3,31 @@ from django.views import View
 from django.http import HttpResponseRedirect
 from .forms import ProfileForm
 from .models import UserProfile
+from django.views.generic.edit import CreateView
 
 
-class CreateProfileView(View):
-    def get(self, request):
-        form = ProfileForm()
+class CreateProfileView(CreateView):
+    template_name = 'profiles/create_profile.html'
+    model = UserProfile
+    fields = '__all__'
+    success_url = '/profiles'
 
-        return render(request, "profiles/create_profile.html", {
-            'form': form
-        })
 
-    def post(self, request):
-        submitted_form = ProfileForm(request.POST, request.FILES)
-        if submitted_form.is_valid():
-            profile = UserProfile(image=request.FILES['user_image'])
-            profile.save()
-            return HttpResponseRedirect('/profiles/')
-
-        return render(request, "profiles/create_profile.html", {
-            'form': submitted_form
-        })
+# class CreateProfileView(View):
+#     def get(self, request):
+#         form = ProfileForm()
+#
+#         return render(request, "profiles/create_profile.html", {
+#             'form': form
+#         })
+#
+#     def post(self, request):
+#         submitted_form = ProfileForm(request.POST, request.FILES)
+#         if submitted_form.is_valid():
+#             profile = UserProfile(image=request.FILES['user_image'])
+#             profile.save()
+#             return HttpResponseRedirect('/profiles/')
+#
+#         return render(request, "profiles/create_profile.html", {
+#             'form': submitted_form
+#         })

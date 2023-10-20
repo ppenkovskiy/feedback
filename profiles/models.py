c5b2c32e5c85bb16ad544e943c51f49a0ef36267
Pavel Penkovskiy <p.penkovskiy@gmail.com>
2023-10-20 16:39:23 +0300

    24

--- profiles/models.py
+++ profiles/models.py
@@ -3,5 +3,5 @@ from django.db import models
 
 # This model only stores the path to the file on the hard drive
 class UserProfile(models.Model):
-    image = models.FileField(upload_to='images')
+    image = models.ImageField(upload_to='images')
 
